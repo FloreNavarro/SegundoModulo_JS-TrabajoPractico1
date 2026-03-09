9db93117962fb1711a019d5b7f1fcb34369c87d6
Florencia Navarro <fmnavarro1o11@gmail.com>
2026-03-09 02:19:43 -0300

    primera linea de codigo

---
+++ js/05-ejercicio.js
@@ -0,0 +1,5 @@
+//Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+
+const numero1= prompt("Ingrese el primer numero:")
+const numero2= prompt("Ingrese el segundo numero:")
+const suma= alert((numero1 + numero2))
