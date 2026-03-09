eea94a18a66c076245adfc92b305cc8fcb487255
Florencia Navarro <fmnavarro1o11@gmail.com>
2026-03-09 05:20:37 -0300

    ejercicio terminado

---
+++ js/08-ejercicio.js
@@ -0,0 +1,7 @@
+//Escribe un programa que pida un número y diga si es divisible por 2
+const num1 = Number(prompt("Ingresa un numero: "))
+if(num1 % 2 == 0){
+    alert("El numero ingresado es divisible por 2")
+}else {
+    alert("El numero ingresado NO es divisible por 2")
+}
