589f808268a52f1263d8f0feea0d297a7a382805
Florencia Navarro <fmnavarro1o11@gmail.com>
2026-03-09 03:42:17 -0300

    ejercicio finalizado

---
+++ js/06-ejercicio.js
@@ -0,0 +1,9 @@
+//Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
+const num1 = Number(prompt("Ingresa el primer numero: "))
+const num2 = Number(prompt("Ingresa el segundo numero: "))
+
+if(num1>num2){
+    alert("El primer numero ingresado es MAYOR")
+}else{
+    alert("El segundo numero ingresado es MAYOR")
+}
